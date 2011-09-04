d841259223bc82f6719b48ec4e6e07bb667ca91a
StephanChang <tcmail0111@gmail.com>
2011-09-04 13:48:56 -0400

    Added layout links (with routes)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,5 +10,10 @@ class PagesController < ApplicationController
   def about
     @title ="About"
   end
+  
+  def help
+    @title= "Help"
+  end
+  
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -49,5 +49,22 @@ describe PagesController do
       end
     
   end
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+     it "should have the right title" do
+        get 'help'
+        response.should have_selector("title",
+                        :content =>"Ruby on Rails Tutorial Sample App | Help")
+      end
+    
+  end
+  
+  
+  
 
 end
