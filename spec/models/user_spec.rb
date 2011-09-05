8578c36edb956c5aca6bbe6b20a205ee834111f1
StephanChang <tcmail0111@gmail.com>
2011-09-05 13:19:46 -0400

    Finish the password machinery

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -125,6 +125,48 @@ describe User do
       @user.should respond_to(:encrypted_password)
      end 
      
+    it "should set the encrypted password attribute " do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+    it "should have a salt" do
+      @user.should respond_to(:salt)
+    end
+    
+    describe "has_password? method" do
+      
+      it "should exist" do
+        @user.should respond_to(:has_password?)
+      end
+      
+      it "should be true if the passwords match" do 
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      
+      it "should be false if the passwords don't match" do 
+        @user.has_password?("invalid").should be_false
+      end
+    end
+    
+    describe "authenticate method"do
+    
+      it "should exit" do
+        User.should respond_to(:authenticate)
+      end
+      
+      it "should return nil on email/password mismatch" do
+        User.authenticate(@attr[:email],"wrongpass").should be_nil
+      end
+      
+      it "should return nil for an email address with no user" do
+        User.authenticate("bar@foo.com",@attr[:password]).should be_nil
+      end
+      
+      it "should return the user on email/password match"do
+        User.authenticate(@attr[:email],@attr[:password]).should == @user
+      end
+    
+   end
   end
 
 end
