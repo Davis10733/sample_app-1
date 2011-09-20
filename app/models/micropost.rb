4b75f451f8f6233f43b1406457736d11b03d57e4
StephanChang <tcmail0111@gmail.com>
2011-09-20 13:05:38 -0400

    Micropost validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -16,6 +16,9 @@ class Micropost < ActiveRecord::Base
   
   belongs_to :user
   
+  validates :content, :presence => true, :length=>{ :maximum => 140 }
+  validates :user_id, :presence => true
+  
   default_scope :order =>'microposts.created_at DESC'
   
   
--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -19,7 +19,7 @@ describe Micropost do
     @attr = {:content =>"lorem ipsum",:user_id => 1}
   end
   it "should create a new instance with valid attributes "do
-    Micropost.create!(@attr)
+    @user.microposts.create!(@attr)
   end
    
     describe "user associations" do
@@ -35,6 +35,19 @@ describe Micropost do
       it "should have the right associated user" do
          @micropost.user_id.should ==@user.id
          @micropost.user.should == @user
+      end
+    end
+    describe "validations" do
+      it "should have a user id" do
+        Micropost.new(@attr).should_not be_valid
+      end
+      
+      it "should require nonblank content" do
+        @user.microposts.build(:content=> "     ").should_not be_valid
+      end
+      
+      it "should reject long content" do
+         @user.microposts.build(:content => "a" * 141).should_not be_valid
       end
       
     end
