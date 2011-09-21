ddd3646d2cf3c72e7af30f5968f0a0c1fa01c7f8
StephanChang <tcmail0111@gmail.com>
2011-09-20 22:15:53 -0400

    Microposts interface

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,6 +26,10 @@ module SessionsHelper
     user == current_user
   end
   
+  def authenticate
+    deny_access unless signed_in?
+  end
+  
   def deny_access
       session[:return_to] = request.fullpath
       redirect_to signin_path, :notice =>"Please sign in to access this page." unless signed_in?
