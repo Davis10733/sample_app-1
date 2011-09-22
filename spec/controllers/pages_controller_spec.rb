38c45e49bd9363cd3d6a85454173059061abd820
StephanChang <tcmail0111@gmail.com>
2011-09-22 06:10:19 -0400

    Initial interface for following/followers

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,24 +2,39 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+     @base_title = "Ruby on Rails Tutorial Sample App"
+  end
 
   describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
+    describe "when not signed in" do
+      it "should be successful" do
+        get 'home'
+        response.should be_success
+      end
+      
+      it "should have the right title" do
+        get 'home'
+        response.body.should_not =~ /<body>\*s<\/body>/
+      end
     end
-    
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-                      :content =>"Ruby on Rails Tutorial Sample App | Home")
+        
+    describe "when signed in" do
+       before(:each) do
+          @user = test_sign_in(Factory(:user))
+          other_user = Factory(:user, :email => Factory.next(:email))
+          other_user.follow!(@user)
+        end
+        
+        it "should have the right follower/following counts" do 
+            get :home
+            response.should have_selector('a', :href => following_user_path(@user),
+                                               :content => "0 following")
+            response.should have_selector('a', :href => followers_user_path(@user),
+                                               :content => "1 follower")
+        end
     end
-    
-    it "should have a non-blank body" do
-     get 'home'
-     response.body.should_not =~ /<body>\s*<\/body>/
-    end
-    
   end
 
   describe "GET 'contact'" do
